cdcc0d49090064230a60bff6800d908a709c239c
zzzhan <imailer@sina.com>
2015-09-24 01:21:00 +0800

    compress files to one zip archive

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+var path = require('path');
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -11,24 +12,27 @@ module.exports = function (grunt) {
           'bower_components/shapefly-plugin/dist/shapefly-plugin.min.js',
           'bower_components/tinymce-plugin-shapefly/dist/tinymce-plugin-shapefly.min.js'
         ],
-        dest: 'dist/wp-tinymce-plugin-shapefly.min.js'
+        dest: 'tmp/wp-tinymce-plugin-shapefly.min.js'
       }
     },
-	clean:['dist'],
-    copy: {
-      build: {
-	    files: [{
-		  expand: true,
-		  cwd: 'src',
-		  src: '**/*.php',
-		  dest:'dist/'
-		}, {
-		  'dist/shapefly-plugin.html':'bower_components/shapefly-plugin/dist/shapefly-plugin.min.html',
-		  'dist/tinymce-plugin-shapefly.min.css':'bower_components/tinymce-plugin-shapefly/dist/tinymce-plugin-shapefly.min.css'
-		}]
+	clean:['tmp','dist'],
+	zip: {
+      'skip-path': {
+		router: function (filepath) {
+		  var filename = path.basename(filepath);
+		  return filename;
+		},
+        compression: 'DEFLATE',
+		src:[
+		  'tmp/wp-tinymce-plugin-shapefly.min.js',
+		  'src/wp-tinymce-plugin-shapefly.php',
+		  'bower_components/shapefly-plugin/dist/shapefly-plugin.min.html',
+		  'bower_components/tinymce-plugin-shapefly/dist/tinymce-plugin-shapefly.min.css'
+		],
+		dest: 'dist/wp-tinymce-plugin-shapefly.zip'
 	  }
 	}
   });
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  grunt.registerTask('default', ['clean','concat','copy']);
+  grunt.registerTask('default', ['clean','concat','zip']);
 };
